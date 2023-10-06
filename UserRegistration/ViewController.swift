8d56c4bb58350f791c908764d26e3d9752e5e6ad
mila-carlton <mila.carlton@mail.ru>
2023-10-06 22:01:34 +0400

    Initial Commit

---
+++ UserRegistration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserRegistration
+//
+//  Created by PASGON TEXTILE on 06.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
